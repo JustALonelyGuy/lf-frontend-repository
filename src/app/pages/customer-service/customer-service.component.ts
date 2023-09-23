0276f1ad02322113aa5ba8e32ecb989bbfc7f66d
Cheong Kar Wai <cheongkarwai5@gmail.com>
2023-09-23 14:12:49 +0800

    designing customer service, amendment for homepage

--- src/app/pages/customer-service/customer-service.component.ts
+++ src/app/pages/customer-service/customer-service.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Tab} from "./tab.interface";
+import * as Aos from "aos";
 
 @Component({
   selector: 'app-customer-service',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerServiceComponent implements OnInit {
 
+  selectedTab = "FAQ";
+
+  tabs:Tab[] = [
+    {imageUrl:'../../../assets/images/faq.png',title:'FAQ',width:150,height:150,alt:'FAQ'},
+    {imageUrl:'../../../assets/images/information.png',title:'Terms & Conditions', width:150, height:150,alt:"term & condition"},
+    {imageUrl:'../../../assets/images/exchange.png',title:'Return & Refund Policy', width:150, height:150,alt:"refund"},
+    {imageUrl:'../../../assets/images/mail-message.png',title:'Contact Us', width:150, height:150,alt:"contact"}
+  ]
   constructor() { }
 
   ngOnInit(): void {
+    Aos.init();
+  }
+
+  switchTab(name:any){
+    this.selectedTab = name;
   }
 
 }
---
+++ src/app/pages/customer-service/tab.interface.ts
@@ -0,0 +1,7 @@
+export interface Tab{
+  imageUrl:string;
+  width:number;
+  height:number;
+  title:string;
+  alt:string;
+}
